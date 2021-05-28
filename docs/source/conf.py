6b41e11e2020d13a663141f4d07fc569b98e105c
adrianruizmora <adrianruizmora@hotmail.com>
2021-05-28 17:05:54 +0200

    sphinx docs with readme and automatisation of flask run

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,11 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'recommonmark'
 ]
 napoleon_google_docstring = False
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -49,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'sizzle'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,8 +10,9 @@ Welcome to adc-heroku's documentation!
    :maxdepth: 2
    :caption: Contents:
    
-   readme
+   README.md
    modules
+
 
 
 
